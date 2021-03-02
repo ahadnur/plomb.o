6d8feb37928cd8630f590535f91c0339d750699f
ahadnur <ahadnur44@gmail.com>
2021-03-03 02:07:12 +0600

    add player updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-let confirm_btn = document.getElementById('confirm');
+// sections
 let warningPage = document.querySelector('.init__warning');
 let homepage = document.querySelector('.homepage');
+let addPlayer = document.querySelector('.add__player');
+
+// warning page btn
+let confirm_btn = document.getElementById('confirm');
+
+// Home Page sections and buttons
+let addPlayerBtn = document.getElementById('add__player');
+
+// btns of add player
+let addPlayerPrevBtn = document.getElementById('homePage');
+let addPlayerNextBtn = document.getElementById('addNewPlayer');
+
 confirm_btn.addEventListener('click', () => {
     warningPage.style.display = 'none';
     homepage.style.display = 'block';
+})
+
+addPlayerBtn.addEventListener('click', ()=> {
+    homepage.style.display = 'none';
+    addPlayer.style.display = 'block';
+})
+
+addPlayerPrevBtn.addEventListener('click', () => {
+    addPlayer.style.display = 'none';
+    homepage.style.display = 'block';
+})
+
+addPlayerNextBtn.addEventListener('click', () => {
+    addPlayer.style.display = "none";
+    
 })
